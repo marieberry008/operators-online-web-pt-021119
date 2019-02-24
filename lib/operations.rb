c5173ed7d758840f2b51721465ccc0ecbe9a6bf8
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-24 21:24:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,8 @@
 def unsafe?(speed)
 
-if 60 < speed < 40
-  puts "true"
- 
-else 60 > speed < 40
-    "false"
+if speed < 40 ; speed > 60 ? "true" : "false"
 end
 
-
-
-def not_safe?(speed)
-	
 end
 	
 # Use an `if`/`else` statement pair to build the `unsafe?` method. It should return `true` if the speed is either below `40` or above `60`. *Going 30 mph on the freeway would be unsafe, as would going 95 mph. Going 50 miles per hour, however, would return `false` as that's within the "safe" range.*
